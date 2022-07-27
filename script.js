f17dd5dff0ef39949bfc7b633bc5d11a1f053ef6
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-07-27 16:19:57 -0600

    add comments to javascript

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+//function so that the DOM loads before this script runs that way there are no bugs
 $(document).ready(function () {
 
 
+// Displaying todays date in specific format
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+// Displaying current time in specific format
 var currentTime = moment();
 $("#currentLocalTime").text(today.format('LT'))
 
 
-
+//call timer function
 timer();
 
+// Set interval for timer to call every 30 seconds
 setInterval(timer, 30000 )
 
 
 
-//need a time function to change the time blocks based on the current time
+// Function to loop through each time block and change design attribute based on current time
 function timer() {
   let currentHour = moment().hour()
   $(".time-block").each(function () {
@@ -33,12 +37,14 @@ function timer() {
   })
 }
 
-// need a function to save items in to local storage that are entered into a block
+// Function for save button to save textarea content into local storage
 $(".saveBtn").on("click", function () {
   let value = $(this).siblings(".description").val()
   let time = $(this).parent().attr("id")
   localStorage.setItem(time, value)
 })
+
+
 
 $("#9 .description").val(localStorage.getItem("9"))
 $("#10 .description").val(localStorage.getItem("10"))
